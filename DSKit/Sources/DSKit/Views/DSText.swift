6d40ff21ac2568e78f5c4cd98bdebee003d44465
Ivan Borinschi <ivan.borinschi@Ivans-Mac-mini.local>
2026-02-13 15:53:50 +0200

    Fix DSText dynamic type scaling and refresh snapshots

--- DSKit/Sources/DSKit/Views/DSText.swift
+++ DSKit/Sources/DSKit/Views/DSText.swift
@@ -26,6 +26,7 @@ public struct DSText: View {
     
     @Environment(\.appearance) var appearance: DSAppearance
     @Environment(\.viewStyle) var viewStyle: DSViewStyle
+    @Environment(\.sizeCategory) var sizeCategory: ContentSizeCategory
     @Environment(\.textStyle) var textStyle: DSTextStyle
     
     let text: String
@@ -40,7 +41,7 @@ public struct DSText: View {
     
     public var body: some View {
         Text(text)
-            .font(textStyle.font(for: appearance))
+            .font(textStyle.font(for: appearance, sizeCategory: sizeCategory))
             .foregroundStyle(textStyle.color(for: appearance, and: viewStyle))
             .multilineTextAlignment(alignment)
             .lineSpacing(lineSpacing)
@@ -116,7 +117,36 @@ public indirect enum DSTextStyle: Equatable, Hashable {
     }
     
     func font(for appearance: DSAppearance) -> Font {
-        dsTextFont.font(for: appearance)
+        font(for: appearance, sizeCategory: nil)
+    }
+    
+    func font(
+        for appearance: DSAppearance,
+        sizeCategory: ContentSizeCategory?
+    ) -> Font {
+        Font(uiFont(for: appearance, sizeCategory: sizeCategory))
+    }
+    
+    private func uiFont(
+        for appearance: DSAppearance,
+        sizeCategory: ContentSizeCategory?
+    ) -> DSFont {
+        #if canImport(UIKit)
+        let uiFont = dsTextFont.uiFont(for: appearance)
+        guard let sizeCategory,
+              let textStyle = dsTextFont.uiTextStyle()
+        else {
+            return uiFont
+        }
+        
+        return UIFontMetrics(forTextStyle: textStyle)
+            .scaledFont(
+                for: uiFont,
+                compatibleWith: UITraitCollection(preferredContentSizeCategory: sizeCategory.uiContentSizeCategory)
+            )
+        #else
+        return dsTextFont.uiFont(for: appearance)
+        #endif
     }
     
     func color(for appearance: DSAppearance, and viewStyle: DSViewStyle) -> Color {
@@ -127,6 +157,78 @@ public indirect enum DSTextStyle: Equatable, Hashable {
         dsTextFont.uiFont(for: appearance).pointSize
     }
 }
+
+#if canImport(UIKit)
+private extension ContentSizeCategory {
+    var uiContentSizeCategory: UIContentSizeCategory {
+        switch self {
+        case .extraSmall:
+            .extraSmall
+        case .small:
+            .small
+        case .medium:
+            .medium
+        case .large:
+            .large
+        case .extraLarge:
+            .extraLarge
+        case .extraExtraLarge:
+            .extraExtraLarge
+        case .extraExtraExtraLarge:
+            .extraExtraExtraLarge
+        case .accessibilityMedium:
+            .accessibilityMedium
+        case .accessibilityLarge:
+            .accessibilityLarge
+        case .accessibilityExtraLarge:
+            .accessibilityExtraLarge
+        case .accessibilityExtraExtraLarge:
+            .accessibilityExtraExtraLarge
+        case .accessibilityExtraExtraExtraLarge:
+            .accessibilityExtraExtraExtraLarge
+        @unknown default:
+            .medium
+        }
+    }
+}
+
+private extension DSTextFontKey {
+    func uiTextStyle() -> UIFont.TextStyle? {
+        switch self {
+        case .largeTitle:
+            return .largeTitle
+        case .title1:
+            return .title1
+        case .title2:
+            return .title2
+        case .title3:
+            return .title3
+        case .headline:
+            return .headline
+        case .subheadline:
+            return .subheadline
+        case .body:
+            return .body
+        case .callout:
+            return .callout
+        case .caption1:
+            return .caption1
+        case .caption2:
+            return .caption2
+        case .footnote:
+            return .footnote
+        case .custom:
+            return nil
+        case .fontWithSize(let style, _):
+            return style.uiTextStyle()
+        case .smallHeadline, .largeHeadline:
+            return .headline
+        case .smallSubheadline:
+            return .subheadline
+        }
+    }
+}
+#endif
 
 struct DSTextStyleEnvironment: EnvironmentKey {
     static let defaultValue: DSTextStyle = .textFont(.body)
